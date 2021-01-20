4c5de03a8d1d5e90823ab59698c3b7128153b2ca
TheECanyon <schindler89@aol.com>
2021-01-20 18:34:32 +0100

    Add JavaDoc / JML

--- src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/sheet08/homework/olympics/FeedTwiceStrategy.java
+++ src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/sheet08/homework/olympics/FeedTwiceStrategy.java
@@ -1,8 +1,21 @@
 package de.unistuttgart.iste.rss.oo.hamstersimulator.sheet08.homework.olympics;
 
+
+/**
+ * Algorithm for the FeedingStrategy to eat two grains per grain storage tile
+ * @author Alex, Max, Jannick
+ * @version 0.1.0.20210120_1816
+ */
 public class FeedTwiceStrategy implements FeedingStrategy {
     int grainsTaken = 1;
-    @Override public boolean isFeedingRequired() {
+
+    /**
+     * This method returns true as long as 1 or less grains have been eaten once the hamster entered
+     * the grain storage tile. After eating 2 grains the return value is changed to false.
+     * @return boolean is it required to eat
+     */
+    @Override
+    public boolean isFeedingRequired() {
 
 	if (grainsTaken==1) {
 		grainsTaken++;
